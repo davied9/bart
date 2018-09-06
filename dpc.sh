8ed7bb36f64cb6196ebc4e741d072e2648f77be4
Wei.Wu <Wei.Wu@alltechmed.com>
2018-09-06 14:59:04 +0800

    pass all arguments from dpc.sh to make.sh

--- dpc.sh
+++ dpc.sh
@@ -8,5 +8,5 @@ find . -name "*.c"  | xargs dos2unix 2>&1 | tee -a buildlog.log
 find . -name "*.h"  | xargs dos2unix 2>&1 | tee -a buildlog.log
 find . -name "Makefile" | xargs dos2unix 2>&1 | tee -a buildlog.log
 
-./make.sh 2>&1 | tee -a buildlog.log
+./make.sh $* 2>&1 | tee -a buildlog.log
 
