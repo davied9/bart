faf5039f1a03e503ca155085e209af656fc3e5ae
Wei.Wu <Wei.Wu@alltechmed.com>
2018-09-06 13:43:53 +0800

    add configuration for make.sh

--- make.sh
+++ make.sh
@@ -1,2 +1,26 @@
-NOLAPACKE=1 DEBUG=1 OPENBLAS=1 GFORTRAN=1 ADDITIONAL_LIBRARY=/usr/local/lib64:/usr/local/lib make
+#!/bin/bash
+
+export NOLAPACKE=1
+export OPENBLAS=1
+export GFORTRAN=1
+export ADDITIONAL_LIBRARY=/usr/local/lib64:/usr/local/lib
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --debug )
+            export DEBUG=1
+            ;;
+        -L )
+            shift
+            if [ "$1" == "" ]; then
+                echo -L is specified, library path should be followed, abort.
+                exit
+            fi
+            ADDITIONAL_LIBRARY+=:$1
+    esac
+    shift
+done
+
+
+make
 
